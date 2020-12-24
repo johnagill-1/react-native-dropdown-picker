7fdeaf55729563f1bc66549f52ef16a77be4634b
johnagill-1 <74569861+johnagill-1@users.noreply.github.com>
2020-12-24 16:13:19 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -472,7 +472,7 @@ class DropDownPicker extends React.Component {
                                 }}
                             >
                                 <TouchableOpacity
-                                    testID={this.props.testID}
+                                    testID={this.getLabel(item)}
                                     key={index}
                                     onPress={() => this.select(item)}
                                     style={[styles.dropDownItem, this.props.itemStyle, (
